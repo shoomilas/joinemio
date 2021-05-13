09e8d041709389afb6d34c0d45d318a4d9358274
Karol Szeliga <63848317+KarolSzeliga4@users.noreply.github.com>
2021-05-13 23:39:23 +0200

    update env using: Game, Board, GameWindow

--- connect_four_env.py
+++ connect_four_env.py
@@ -1,18 +1,30 @@
 import gym
 import enum
 import random
-from gym.spaces import Discrete, Box
+import logging as log
 import numpy as np
+from gym.spaces import Discrete, Box
+from Board import Game, Board, GameState
+from game_window import GameWindow
 
 
 class RandomPlayer:
-    def get_action(self, board_state, my_token):
-        return random.choice(possible_moves(board_state))
+    def get_action(self, board_state):
+        return random.choice(self.possible_moves(board_state))
+
+    @staticmethod
+    def possible_moves(board_state):
+        available_cols = []
+        for i in range(len(board_state)):
+            if board_state[0][i] == 0:
+                available_cols.append(i)
+        return available_cols
 
 
 class AIPlayer:
-    def get_action(self, board_state, my_token):
-        ...
+    def get_action(self, board_state, my_token):  # TODO
+        pass
+
 
 class Reward(enum.Enum):
     win = 100
@@ -20,50 +32,41 @@ class Reward(enum.Enum):
     draw = 1
     not_end = 0
 
+
 class ConnectFourEnv(gym.Env):
     def __init__(self):
+        self.game = Game()
+        self.game_window = GameWindow(self.game)
         self.action_space = Discrete(7)
-        self.observation_space = Box(low=0, high=2, shape=(6, 7), dtype=np.integer)
-        self._board_state = np.zeros((6, 7), dtype=int)
-        self._current_player = 1
+        self.observation_space = Box(low=0, high=2, shape=Board.shape, dtype=np.ushort)
 
-    def play_one_game(self, player1, player2):
-        self.reset()
-        done = False
-        while not done:
-            if self._current_player == 1:
-                self._board_state, reward, done, info = self._step(player1.get_action(self._board_state, 1))
-                self._current_player = 2
-            else:
-                self._board_state, reward, done, info = self._step(player2.get_action(self._board_state, 2))
-                self._current_player = 1
-        return self._board_state, reward, done, info
+    def play_one_game(self, player1_main, player2_opponent):
+        players = [player1_main, player2_opponent]
+        self.observation_space = self.reset()
+        while not self.game.game_state == GameState.finished:
+            self.observation_space, reward, done, info = \
+                self.step(players[self.game.current_player-1].get_action(self.observation_space))
+        log.debug(
+            f"Winner: {self.game.winner}"
+        )
+        return self.observation_space, reward, True, info  # reward for player1
 
-    def _step(self, action):
-        board_state = drop_piece(self._board_state, action)  # function that drops one piece in chosen column
-        reward = self.step_result()
-        if reward == Reward.not_end:
-            return board_state, reward, False, {}  # game not ended
-        elif reward == Reward.win:
-            if self._current_player == 1:
-                return board_state, reward, True, {}  # player1 wins
+    def step(self, action):  # close to move from Game class
+        self.game.move(action)  # switching player if game not ended
+        if self.game.game_state == GameState.finished:
+            if self.game.winner == 1:
+                return self.observation_space, Reward.win, False, {}
+            elif self.game.winner == 2:
+                return self.observation_space, Reward.loss, False, {}
             else:
-                return board_state, -reward, True, {}  # player2 wins
+                return self.observation_space, Reward.draw, False, {}
         else:
-            return board_state, reward, True, {}  # draw
-
-    def step_result(self):
-        if end_state(self._board_state):  #some function
-            if win_state(self._board_state):  #some function
-                return Reward.win
-            else:
-                return Reward.draw
-        else:
-            return Reward.not_end
+            return self.observation_space, Reward.not_end, False, {}  # not end
 
     def reset(self):
-        self._board_state = np.zeros((6, 7), dtype=int)
-        return self.state
-    
-    def render(self):
-        draw_pieces_in_window(self._board_state)
+        self.game = Game()
+        return self.game.board.grid
+
+    def render(self, mode: str = 'console', close: bool = False) -> None:
+        self.game_window.on_draw()
+

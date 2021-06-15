e883f2762dd13490748d0738158c873b2b2eeb50
shoomilas <jakub.szumilas@gmail.com>
2021-06-16 00:45:25 +0200

    Package prep

---
+++ gym-joinemio/gym_joinemio/__init__.py
@@ -0,0 +1,10 @@
+from gym.envs.registration import register
+
+register(
+    id='joinemio-v0',
+    entry_point='gym_joinemio.envs:ConnectFourEnv',
+)
+# register(
+#     id='foo-extrahard-v0',
+#     entry_point='gym_foo.envs:FooExtraHardEnv',
+# )
---
+++ gym-joinemio/gym_joinemio/envs/__init__.py
@@ -0,0 +1,3 @@
+from gym_joinemio.envs.connect_four_env import ConnectFourEnv
+from gym_joinemio.envs.player import RandomPlayer
+from gym_joinemio.envs.player import AIPlayer

816e4bb2c761362efd527319b81106b47148e39a
shoomilas <jakub.szumilas@gmail.com>
2021-08-20 19:16:08 +0200

    It learns!

--- gym-joinemio/gym_joinemio/envs/connect_four_env.py
+++ gym-joinemio/gym_joinemio/envs/connect_four_env.py
@@ -34,9 +34,14 @@ class GameWindowForBots(GameWindow):
 
 class Reward(enum.Enum):
     win = 1
-    loss = -10
-    draw = -1
-    not_end = -1/42 # TODO: set to 0 if rewarding AFTER game ends, set to -LOW_VALUE to learn how to end games quicker
+    loss = -1
+    draw = 0
+    not_end = 0
+    # TODO: Choose final rewards
+    # win = 1
+    # loss = -10
+    # draw = -1
+    # not_end = -1/42 # TODO: set to 0 if rewarding AFTER game ends, set to -LOW_VALUE to learn how to end games quicker
 
 
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
